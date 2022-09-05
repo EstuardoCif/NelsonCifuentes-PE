e7065355b69bfe2d5acc7534c7d6cb7603899230
EstuardoCif <112735916+EstuardoCif@users.noreply.github.com>
2022-09-05 11:49:02 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql, {
     host: 'localhost',
     user: 'root',
-    password: 'Goleador10',
+    password: '',
     port: 3306,
     database: 'crudcar'
 }, 'single'));
